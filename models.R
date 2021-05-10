2f06fc5a42b1f645840a5b839ddeaee220075a30
Joselyn Rodriguez <joselyn.marie.rodriguez@gmail.com>
2021-05-10 12:02:10 -0400

    whoops need data to work

--- models.R
+++ models.R
@@ -1,9 +1,16 @@
 library(brms)
+library(tidyverse)
 
 ########################################################################################################################
 # This file is for running the bayesian models and saving the models so I don't have to run them each time             #
 ########################################################################################################################
 
+# need to load in data again
+test <- read_csv(here::here("data_tidy/data_test_tidy.csv")) %>%
+  mutate(block = factor(block, levels=c("pre", "post")),   # re-factor to set reference levels
+         condition = factor(condition, levels = c("tent-biasing", "dent-biasing")),
+         resp_t = as.numeric(resp_t))  %>%
+  mutate(vot = vot - mean(vot))  # center vot
 
 
 #### Bayesian Modeling 
